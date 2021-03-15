e6096a8b3a7c13f879a79b8ea22cdfcdf6d046db
Documenter.jl <documenter@juliadocs.github.io>
2021-03-15 19:27:34 +0000

    build based on 52201b3

--- dev/generated/benchmarks.jl
+++ dev/generated/benchmarks.jl
@@ -12,29 +12,29 @@ rand_Ψ = rand(FT) - 3;
 gas_air = TraceGasAir();
 gas_CO₂ = TraceGasCO₂();
 
-println("\nBenchmarking capillary_pressure functions...");
+@info "Benchmarking capillary_pressure functions...";
 @btime capillary_pressure($rand_r, $rand_T);
 @btime capillary_pressure($rand_r, $rand_T, $rand_α);
 
-println("\nBenchmarking diffusive_coefficient functions...");
+@info "Benchmarking diffusive_coefficient functions...";
 @btime diffusive_coefficient($rand_T, $gas_CO₂, $gas_air);
 @btime relative_diffusive_coefficient($rand_T);
 
-println("\nBenchmarking latent_heat_vapor functions...");
+@info "Benchmarking latent_heat_vapor functions...";
 @btime latent_heat_vapor($rand_T);
 
-println("\nBenchmarking saturation_vapor_pressure functions...");
+@info "Benchmarking saturation_vapor_pressure functions...";
 @btime pressure_correction($rand_T, $rand_Ψ);
 @btime saturation_vapor_pressure($rand_T);
 @btime saturation_vapor_pressure($rand_T, $rand_Ψ);
 @btime saturation_vapor_pressure_slope($rand_T);
 @btime saturation_vapor_pressure_slope($rand_T, $rand_Ψ);
 
-println("\nBenchmarking surface_tension functions...");
+@info "Benchmarking surface_tension functions...";
 @btime surface_tension($rand_T);
 @btime relative_surface_tension($rand_T);
 
-println("\nBenchmarking viscosity functions...");
+@info "Benchmarking viscosity functions...";
 @btime viscosity($rand_T);
 @btime relative_viscosity($rand_T);
 
